bdb38c360251f3b6cac17e1e38fb354c1a25affe
linhchi0508 <dang.linh.chi@sun-asterisk.com>
2021-08-16 14:07:31 +0700

    Add file migration + model

---
+++ quickstart/app/Models/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Subject;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'email',
+        'fullname',
+        'avatar',
+        'student_number',
+        'major',
+        'birthday',
+        'phone',
+    ];
+
+    public function subjects(){
+        return $this->belongsToMany(Subject::class, 'student_subject', 'subject_id', 'student_id');
+    }
+
+}
---
+++ quickstart/app/Models/Subject.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Student;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'major',
+        'subject_code',
+    ];
+
+    public function students(){
+        return $this->belongsToMany(Student::class, 'student_subject', 'student_id', 'subject_id');
+    }
+}
