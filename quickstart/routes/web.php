f8ec71ac28f2b8d25a2b106bc528a761a23ad760
linhchi0508 <dang.linh.chi@sun-asterisk.com>
2021-08-18 15:34:37 +0700

    Add update subject

--- quickstart/routes/web.php
+++ quickstart/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('students', 'App\Http\Controllers\StudentController');
+Route::resource('subjects', 'App\Http\Controllers\SubjectController');
+Route::post('students/point/{id}', 'App\Http\Controllers\StudentController@createPoint')->name('point.create');
+Route::delete('students/{student_id}/point/{subject_id}','App\Http\Controllers\StudentController@destroyPoint')->name('point.destroy');
+Route::get('language/{locale}', 'App\Http\Controllers\LanguageController@index')->name('language.index');
